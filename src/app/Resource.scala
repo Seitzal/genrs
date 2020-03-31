577f9f6738c76d6f8f360414117287910a392f05
Alex Seitz <alex.seitz1@gmx.net>
2020-04-01 00:43:49 +0200

    Add documentation to Resource typeclass

--- src/app/Resource.scala
+++ src/app/Resource.scala
@@ -8,25 +8,38 @@ import scala.util.Try
  */
 trait Resource[T : ClassTag]
 
+  /** Whether a resource consists of multiple other resources */
   def isCompound(t: T): Boolean
 
+  /** String uniquely identifying a resource type.
+   *  In the interest of maintaining uniqueness, non-base rtypes should be
+   *  prefixed with their org and project, i.e. "eu.seitzal.fooproject.bartype".
+   */
   def rtype(t: T): String
 
+  // TODO: Remove, as this serves no purpose.
+  /** Textual representation of the resource. */
   def textual(t: T): String
 
+  /** Serialise a resource as a JSON string. */
   def toJson(t: T): String
 
+  /** Try to derive a resource from a uJson AST */
   def fromJson(json: ujson.Value): Try[T]
 
+  /** Derive a resource from a uJson AST */
   def fromJsonUnsafe(json: ujson.Value): T =
     fromJson(json).get
 
+  /** Alias function to deserialize a uJson AST from a JSON string. */
   private def readJsonUnsafe(str: String) = 
     ujson.read(ujson.Readable.fromString(str))
 
+  /** Deserialize a resource from a JSON string */
   def fromJsonUnsafe(str: String): T =
     fromJsonUnsafe(readJsonUnsafe(str))
   
+  /** Try to deserialize a resource from a JSON string */
   def fromJson(str: String): Try[T] =
     Try(readJsonUnsafe(str)).flatMap(fromJson)
 
