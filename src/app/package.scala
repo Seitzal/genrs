b2cf4bbec3a17233191247d0fb3f28c7a8fea46d
Alex Seitz <alex.seitz1@gmx.net>
2020-04-25 17:28:29 +0200

    Clean up for rewrite

--- src/app/package.scala
+++
@@ -1,6 +0,0 @@
-/** The root package of GenRS. Contains by way of trait inheritance all implicit
- *  extension methods defined in the package.
- */
-package object genrs
-  extends GlobalExtensions
-  with ResourceExtensions
